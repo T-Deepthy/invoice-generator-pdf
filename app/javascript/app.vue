17889c76608c0f80b4563e25311e3a55559b09d9
T-Deepthy <deeps1929@gmail.com>
2019-07-16 17:12:20 +0530

    Delete hello_vue.js Create navigation bar Create textfileds Create table

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,124 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand href="#">Invoice Generator</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item>HI, THIS IS FREE INVOICE GENERATOR</b-nav-item>
+        <b-button variant="light" href="https://invoicely.com/">Learn More</b-button>
+        <b-button href="https://invoicely.com/signup">Sign Up</b-button>
+      </b-navbar-nav>
+    </b-navbar>
+    <b-container fluid class="background-color">
+      <b-row class="ml-auto">
+        <b-col align-self="center" cols="4">first patn</b-col>
+        <b-col align-self="end" cols="8">
+          <b-container>
+            <b-row>
+              <b-col sm="5">
+                <b-form-input id="input-small" size="sm" placeholder="Your company name"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-nav-item>INVOICE</b-nav-item>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="input-small" size="sm" placeholder="Your street"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="input-small" size="sm" placeholder="Date"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="input-small" size="sm" placeholder="Your town"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="input-small" size="sm" placeholder="Invoice number"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="input-large" size="sm" placeholder="Address line 3"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="input-large" size="sm" placeholder="PO 682026"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="imput-small" size="sm" placeholder="Phone number"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="imput-small" size="sm" placeholder="Name"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="imput-small" size="sm" placeholder="Email id"></b-form-input>
+              </b-col>
+              <b-col sm="5">
+                <b-form-input id="imput-small" size="sm" placeholder="Client Company Name"></b-form-input>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <b-form-textarea
+                  id="textarea"
+                  v-model="text"
+                  placeholder="Enter something..."
+                  rows="3"
+                  max-rows="6"
+                ></b-form-textarea>
+              </b-col>
+            </b-row>
+            <div>
+              <b-table small :fields="fields" :items="items">
+                <template slot="index" slot-scope="data"> {{ data.index + 1 }}</template>
+                <template slot="item"><b-form-input placeholder="item descp"></b-form-input></template>
+                <template slot="qty"><b-form-input placeholder="0"></b-form-input></template>
+                <template slot="up"><b-form-input placeholder="0"></b-form-input></template>
+                <template slot="tot"><b-form-input placeholder="0"></b-form-input></template>
+              </b-table>
+            </div>
+            <b-row>
+              <b-col>
+                <b-form-textarea
+                  id="textarea"
+                  v-model="text"
+                  placeholder="Enter something..."
+                  rows="3"
+                  max-rows="6"
+                ></b-form-textarea>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data() {
     return {
-      message: "Hello Vue!"
-    }
+      fields: [
+        "index",
+        { key: "item", label: "Item Description" },
+        { key: "qty", label: "Quantity" },
+        { key: "up", label: "Unit Price" },
+        { key: "tot", label: "Total" },
+      ],
+      items: [
+        { name: { first: "John", last: "Doe" }, sex: "Male", age: 42 },
+        { name: { first: "Jane", last: "Doe" }, sex: "Female", age: 36 },
+        { name: { first: "Rubin", last: "Kincade" }, sex: "Male", age: 73 },
+        {
+          name: { first: "Shirley", last: "Partridge" },
+          sex: "Female",
+          age: 62
+        }
+      ],
+      text: ""
+    };
   }
-}
+};
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+.background-color {
+  background-color: #200c50;
+  height: 100vh;
 }
 </style>
