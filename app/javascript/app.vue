25413ed155047b8c0e83c2f8525b202b8b6fdd38
T-Deepthy <deeps1929@gmail.com>
2019-08-05 18:22:42 +0530

    Change HSN/SSN to HSSN/SAC

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-   <app-navigation></app-navigation>
+    <b-navbar type="dark" variant="info">
+      <b-navbar-brand href="#">This is a free <strong>GST</strong> invoice generator</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-button variant="light" @click="generatePdf()">Generate PDF</b-button>
+      </b-navbar-nav>
+    </b-navbar>
     <b-container fluid class="background-color">
       <b-col>
         <b-container ref="content">
           <b-row>
-             <b-col class="mt-5" sm="6">
+            <b-col class="mt-5" sm="6">
               <b-input-group size="sm" class="mt-2">
                 <b-form-input id="id1 " :class="validation? 'border-red': ''" size="sm" placeholder="Your company name" v-model="cdetails[0].cname"></b-form-input>
               </b-input-group>
@@ -25,9 +30,10 @@
               <b-input-group size="sm" class="mt-2">
                 <b-form-input id="id7" :class="validation? 'border-red': ''" size="sm" placeholder="State Code" v-model="cdetails[0].state_code"></b-form-input>
               </b-input-group>
-                 </template>      
-              </b-col>
-             <b-col sm="6">
+                 </template>
+                  
+            </b-col>
+            <b-col sm="6">
               <h3 class="mt-2" v-html="status? 'TAX INVOICE' : 'INVOICE' ">TAX INVOICE</h3>
               <b-input-group size="sm" class="mt-2">
                 <datepicker placeholder="Select Date" format="dd MMM yyyy" v-model="cldetails[0].date"></datepicker>
@@ -48,6 +54,7 @@
                 <b-form-input id="id3" size="sm" placeholder="Pin Code" v-model="cldetails[0].po"></b-form-input>
               </b-input-group>
                  <template v-if="status==true">
+             
               <b-input-group size="sm" class="mt-2">
                 <b-form-input id="id7" size="sm" placeholder="GST Number" v-model="cldetails[0].gst_no"></b-form-input>
               </b-input-group>
@@ -233,10 +240,6 @@
   </div>
 </template>
 <script>
-import AppNavigation from "./AppNavigation";
-import CompanyDetails from "./CompanyDetails";
-import ClientDetails from "./ClientDetails";
-import InvoiceCheckbox from "./InvoiceCheckbox"
 import Datepicker from "vuejs-datepicker";
 import { VueEditor } from "vue2-editor";
 import jsPDF from "jspdf";
@@ -245,11 +248,7 @@ import axios from "axios";
 export default {
   components: {
     Datepicker,
-    VueEditor,
-    AppNavigation,
-    CompanyDetails,
-    ClientDetails,
-    InvoiceCheckbox
+    VueEditor
   },
   data() {
     return {
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,10 @@ import Vue from 'vue'
 import App from '../app.vue'
 import BootstrapVue from 'bootstrap-vue'
 import Vue2Editor from "vue2-editor"
+import Vuex from 'vuex'
 Vue.use(BootstrapVue)
 Vue.use(Vue2Editor);
-
+Vue.use(Vuex)
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     render: h => h(App),
