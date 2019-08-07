e605280428b0042117292cb972771a6fe27d0354
T-Deepthy <deeps1929@gmail.com>
2019-08-07 12:51:47 +0530

    Modify script tags of appnavigation, header, table

--- app/javascript/AppNavigation/appnavigation.js
+++ app/javascript/AppNavigation/appnavigation.js
@@ -1,84 +1,15 @@
+import { mapState } from "vuex";
 import Datepicker from "vuejs-datepicker";
 import { VueEditor } from "vue2-editor";
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
-import axios from "axios";
 export default {
   components: {
     Datepicker,
     VueEditor,
   },
-  data() {
-    return {
-      status: false,
-      validation: false,
-      fields: [
-        "index",
-        { key: "sn", label: "HSN/SAC" },
-        { key: "item", label: "Item Description" },
-        { key: "qty", label: "Quantity" },
-        { key: "up", label: "Unit Price" },
-        { key: "tot", label: "Total" }
-      ],
-      items: [
-        { sn: "998434", item: "Software Product", qty: 0, up: 200, tot: 0 }
-      ],
-      cdetails: [
-        {
-          cname: "",
-          street: "",
-          town: "",
-          address: "",
-          phno: "",
-          email: "",
-          state_code: "",
-          gst_no: ""
-        }
-      ],
-      cldetails: [
-        {
-          date: new Date(),
-          ino: "",
-          po: "",
-          name: "",
-          clname: "",
-          state_code: "",
-          gst_no: "",
-          address: ""
-        }
-      ],
-      text1: "Thanks for your service",
-      text2: "Payment terms: to be received in 5 days",
-      customToolbar: [
-        ["bold", "italic", "underline"],
-        [{ list: "ordered" }, { list: "bullet" }]
-      ],
-      footer: [
-        {
-          subTotalText: "Sub Total",
-          subTotal: 0,
-          newsubTotal: 0,
-          discountText: "Discount",
-          discount: 0,
-          discountPercentage: 0,
-          cgstText: "CGST",
-          cgstPercentage: 9,
-          cgst: 0,
-          sgstText: "SGST",
-          sgstPercentage: 9,
-          sgst: 0,
-          igstText: "IGST",
-          igstPercentage: 18,
-          igst: 0,
-          kfcText: "KFC",
-          kfcPercentage: 1,
-          kfc: 0,
-          grandTotalText: "Grand Total",
-          grandTotal: 0
-        }
-      ]
-    };
-  },
+  computed: {
+		...mapState('invoice', ['status', 'validation', 'footer','cldetails', 'cdetails','items'])
+	},
+ 
   watch: {
     footer: {
       deep: true,
--- app/javascript/Header/header.js
+++ app/javascript/Header/header.js
@@ -3,13 +3,8 @@ import Datepicker from "vuejs-datepicker";
 import { VueEditor } from "vue2-editor";
 
 export default {
-  data() { 
-    return { 
-      status: false
-    }
-  },
 	computed: {
-		...mapState('invoice', ['status', 'validation', 'cldetails','cdetails'])
+		...mapState('invoice', ['validation', 'cldetails', 'cdetails'])
 	},
 	components: {
     Datepicker,
@@ -18,73 +13,12 @@ export default {
   data()
   { 
     return {
-      status: false, 
+      status: false,
       text1: "Thanks for your service",
        customToolbar: [
         ["bold", "italic", "underline"],
         [{ list: "ordered" }, { list: "bullet" }]
       ],
-      cdetails: [
-        {
-          cname: "",
-          street: "",
-          town: "",
-          address: "",
-          phno: "",
-          email: "",
-          state_code: "",
-          gst_no: ""
-        }
-      ],
-      cldetails: [
-        {
-          date: new Date(),
-          ino: "",
-          po: "",
-          name: "",
-          clname: "",
-          state_code: "",
-          gst_no: "",
-          address: ""
-        }
-      ],
-
     }
-  },
-  methods:
-  { 
-    addItemToTable() {
-      let row = { sn:'-', item: "", qty: 0, up: 0, tot: 0 };
-      this.items.push(row);
-    },
-    removeLastItem() {
-      this.items.pop();
-    },
-
-    generatePdf() {
-      if (this.validatePDF())
-      {
-        var is_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-        let url =`/invoice/invoice.pdf?cdetails=${JSON.stringify(this.cdetails)}&cldetails=${JSON.stringify(this.cldetails)}&items=${JSON.stringify(this.items)}&text1=${JSON.stringify(this.text1)}&text2=${JSON.stringify(this.text2)}&footer=${JSON.stringify(this.footer)}&status=${JSON.stringify(this.status)}`
-        if(is_chrome)  
-        {
-          const link = document.createElement('a');
-          link.href = url;
-          link.click();
-        }
-      
-        else
-        {
-            this.validation = false
-            window.open(`/invoice/invoice.pdf?cdetails=${JSON.stringify(this.cdetails)}&cldetails=${JSON.stringify(this.cldetails)}&items=${JSON.stringify(this.items)}&text1=${JSON.stringify(this.text1)}&text2=${JSON.stringify(this.text2)}&footer=${JSON.stringify(this.footer)}&status=${JSON.stringify(this.status)}`);
-        }
-      }
-      else
-      {
-          window.alert("Missing fields");
-          this.validation = true
-      }
-    },
   }
-  
 }
